3ee467611b5bb15958c6386430a52ad59f9b0f4b
Alex Sergeev <sanhokk@yandex.ru>
2023-10-13 18:33:25 +0300

    New commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,19 @@
 fun main() {
-    println(countComission("Maestro", 20000, 60000))
+    countComission("Maestro", 700000, 60000)
 }
 
 
 fun countComission(card: String = "VK Pay", lastSum: Int = 0, transfer: Int): Int {
     var comission = 0
+
     if((card == "Mastercard" || card == "Maestro") && lastSum + transfer < 75000) comission = 0
     else if(card == "Mastercard" || card == "Maestro") {comission = transfer * 6/1000}
     else if((card == "Visa" || card == "Мир") && (transfer * 75/10000) > 35 ) {comission = transfer * 75/10000}
     else if (card == "Visa" || card == "Мир") comission = 35
     else comission = 0
+
+    if (card == "VK Pay" && (lastSum > 40000 || transfer >15000)) println("Перевод невозможен. Лимиты исчерпаны")
+    else if (card != "VK Pay" && (lastSum > 600000 || transfer > 150000)) println("Перевод невозможен. Лимиты исчерпаны")
+    else println(comission)
     return comission
 }
